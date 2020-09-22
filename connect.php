c650cafb99e4d16eccea9eed6f851d43ec20c714
Evelyn Liu <evelynliu16@berkeley.edu>
2020-09-22 11:31:40 -0700

    Error fixed

--- connect.php
+++ connect.php
@@ -8,16 +8,23 @@ if (!empty($_POST['day'])) {
 } else {
 	$day = "无";
 }
-$conn = mysqli_connect('localhost','root','', 'contact_info');
+
+
+
+$conn = mysqli_connect('localhost','evelynliu16','@P!URn;{%F(&', 'contacts_info');
+if (!mysqli_set_charset ($conn, "utf8")) {
+    printf("Error loading utf8", mysqli_error($link));
+    exit();
+}
 if (!$conn) {
 	die("No connection:" . mysqli_connect_error());
 } else {
-	$stmt = $conn->prepare("insert into contact_info(name, phone, day, time, info) values(?, ?, ?, ?, ?)");
+	$stmt = $conn->prepare("insert into contacts_info(name, phone, day, time, info) values(?, ?, ?, ?, ?)");
 	$stmt->bind_param("sisss", $name, $phone, $day, $time, $info);
 	$execval = $stmt->execute();
 	echo $execval;
 	$stmt->close();
 	$conn->close();
-	header("Location:http://localhost//Website/confirmation.html");
+	header("Location:http://www.smxsn.com/confirmation.html");
 }
 ?>
